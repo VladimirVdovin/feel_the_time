4c9b02d893772c74be1d5fbfac18a77e0172826f
Vladimir Vdovin <drumtamtamorchestra@gmail.com>
2023-08-02 07:25:22 +0200

    before navigation

--- feel_the_time/views.py
+++ feel_the_time/views.py
@@ -9,16 +9,18 @@ import base64
 
 buttons = ['Работа', 'Семья', 'Готовить', 'Спорт', 'В пути', 'Ванна', 'Отдых', 'Уборка', 'Есть, пить']
 
+
 def button(request):
     if request.method == 'POST':
         button_value = request.POST.get('button_click')
         current_time = timezone.now()
 
-        penultimate_activity = Time.objects.order_by('-id')[0]
-        difference = current_time - penultimate_activity.time
-        difference_in_sec = difference.total_seconds()
-        penultimate_activity.duration = difference_in_sec
-        penultimate_activity.save()
+        if Time.objects.count() != 0:
+            penultimate_activity = Time.objects.order_by('-id')[0]
+            difference = current_time - penultimate_activity.time
+            difference_in_sec = difference.total_seconds()
+            penultimate_activity.duration = difference_in_sec
+            penultimate_activity.save()
 
         activity = Activities.objects.get_or_create(activity_name=button_value)
         Time.objects.create(name='Vladimir', time=current_time, current_activity=activity[0])
@@ -27,7 +29,6 @@ def button(request):
 
         data = {'current_activity': current_activity,
                 'all': all_activities,
-                'difference_in_sec': difference_in_sec,
                 'buttons': buttons,
                 'graphic': grath()
                 }
@@ -36,23 +37,24 @@ def button(request):
 
         all_activities = Time.objects.order_by('-time')[1:]
         data = {'all': all_activities,
-                'buttons': buttons
+                'buttons': buttons,
+                'graphic': grath()
                 }
         return render(request, 'feel_the_time/main.html', context=data)
+
 
 def grath():
 
     total_time = []
     activities = Activities.objects.all()
+    current_time = timezone.now()
     for obj in activities:
-        sum_result = Time.objects.filter(current_activity=obj).aggregate(Sum('duration'))
-        total_time.append(sum_result['duration__sum'])
+        sum_result = Time.objects.filter(current_activity=obj, time__day=current_time.day).aggregate(Sum('duration'))
+        total_time.append(sum_result['duration__sum'] or 0)
 
+    names = [act.activity_name for act in activities]
 
-    acts = Activities.objects.all()
-    names = [act.activity_name for act in acts]
-
-    plt.figure(figsize=(10, 6))
+    plt.figure(figsize=(9, 5))
     plt.barh(names, total_time)
     plt.xlabel('Время')
     plt.ylabel('Активность')
